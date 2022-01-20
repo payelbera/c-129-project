9ef4c4ecaf09870e8e953d0660f28d5ae0c6bbe6
payelbera <60781680+payelbera@users.noreply.github.com>
2022-01-20 22:04:55 +0530

    Update data_merging_129b.py

--- data_merging_129b.py
+++ data_merging_129b.py
@@ -2,11 +2,11 @@
 import csv
 import pandas as pd
 
-file1 = 'bright_stars.csv'
-file2 = 'unit_converted_stars.csv'
+#put the file names in the 2 variables as below:-
+file1 = ''
+file2 = ''
+# declare 2 empty lists d1 and d2
 
-d1 = []
-d2 = []
 with open(file1,'r',encoding='utf8') as f:
     csv_reader =csv.reader(f)
     
@@ -18,14 +18,15 @@ with open(file2,'r',encoding='utf8') as f:
     
     for i in csv_reader:
         d2.append(i)
+#pick header from d1[0] and put to h1 ref h2 and make it same for h1
 
-h1 = d1[0]
 h2 = d2[0]
 
-p_d1 = d1[1:]
-p_d2 = d2[1:]
 
-h = h1+h2
+#pick planet data from d1[1:] and assign to p_d1 refer to p_d2
+p_d2 = d2[1:]
+#add h1 and h2 and put in variable h
+
 
 p_d =[]
 
